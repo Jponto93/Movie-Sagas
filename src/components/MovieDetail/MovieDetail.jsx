a1d8bc7a339aedd4afa789f4f1b76dacda9db17a
Jordan Ponto <jponto93@gmail.com>
2021-11-06 12:19:35 -0500

    added home button to details view

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,20 +1,27 @@
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
+import { useHistory } from "react-router";
 
-function MovieDetail () {
+function MovieDetail() {
+
     const selectedMovie = useSelector(store => store.selectedMovie)
-    
+    const history = useHistory();
+
     return (
         <section>
             <h1>Selected Movie</h1>
             {
-                selectedMovie.title? (
+                selectedMovie.title ? (
                     <>
-                    <h2>{selectedMovie.title}</h2>
-                    <img src={selectedMovie.poster} alt="" />
+                        <h2>{selectedMovie.title}</h2>
+                        <img src={selectedMovie.poster} alt="" />
+                        <button onClick={() => history.push('/')}>HOME</button>
                     </>
                 ) : (
-                    <p>No movie selected.</p>
+                    <>
+                        <p>No movie selected.</p>
+                        <button onClick={() => history.push('/')}>HOME</button>
+                    </>
                 )
             }
         </section>
