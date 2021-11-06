33c7631ecaab545b20ef291b97e18a2c9381d71d
Jordan Ponto <jponto93@gmail.com>
2021-11-06 15:18:18 -0500

    add movie form working, saves movie data to DB including to joint genre table

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,34 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
 import MovieDetail from '../MovieDetail/MovieDetail';
+import MovieForm from '../MovieForm/MovieForm';
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
-      <Router>        
+      <Router>
+        <nav>
+          <Link to="/">MOVIE LIST</Link>
+        </nav>
+
+        <nav>
+          <Link to="/add">ADD MOVIE</Link>
+        </nav>
+
+
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
+
         <Route path="/details" exact>
           <MovieDetail />
         </Route>
-      
-        {/* Add Movie page */}
+
+        <Route path="/add" exact>
+          <MovieForm />
+        </Route>
       </Router>
     </div>
   );
